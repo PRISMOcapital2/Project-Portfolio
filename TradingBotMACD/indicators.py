a69d7eb28e255c7779db8f1d94c0eac76bc80ae6
BillyRobertson <39294730+BillyRobertson@users.noreply.github.com>
2018-05-16 07:41:44 +0930

    Add files via upload

---
+++ TradingBotMACD/indicators.py
@@ -0,0 +1,20 @@
+class Indicators(object):
+
+    def __init__(self):
+        pass
+
+    def SMA(self, prices, length):
+        if prices:
+            return sum(prices[-length:])/length
+        else:
+            return None
+
+    def EMA(self, price_data, length, previousEMA = False):
+        if len(price_data)<length:
+            return None
+        c = 2/float(length+1)
+        if not previousEMA:
+            return self.EMA(price_data,length,self.SMA(price_data,length))
+        else:
+            return c*price_data[-1]+(1-c)*previousEMA
+        
