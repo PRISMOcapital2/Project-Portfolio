13b0796239f03a0194ef6e72843faa40b9e80bfd
BillyRobertson <39294730+BillyRobertson@users.noreply.github.com>
2019-02-01 10:56:34 +1030

    Add files via upload

---
+++ MeanReverting Blog/Exploration_01022019/credentials.py
@@ -0,0 +1,7 @@
+#usernameL billy1999
+#pwrd versadlib999
+
+
+credentials_exchanges = ['poloniex']
+credentials_assets = ['cryptocurrency']
+credentials_poloniex_assets = ['BTC/USDT','OMG/BTC','ETH/USDT','XMR/USDT','USDT/BTC','LTC/BTC','ZEC/BTC','DASH/BTC','ETH/BTC','XMR/BTC']
